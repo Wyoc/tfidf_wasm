00699c060ceceb30e7019e60ae90d47e612b3483
Wyoc <dirtykartoons@gmail.com>
2022-09-05 20:21:11 +0200

    ✅ add console.log macro as a test

--- src/lib.rs
+++ src/lib.rs
@@ -9,11 +9,25 @@ use wasm_bindgen::prelude::*;
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 #[wasm_bindgen]
+extern "C" {
+    // Use `js_namespace` here to bind `console.log(..)` instead of just
+    // `log(..)`
+    #[wasm_bindgen(js_namespace = console)]
+    fn log(s: &str);
+}
+
+#[macro_export]
+macro_rules! console_log {
+    ($($t:tt)*) => (log(&format_args!($($t)*).to_string()))
+}
+
+#[wasm_bindgen]
 extern {
     fn alert(s: &str);
 }
 
 #[wasm_bindgen]
 pub fn greet() {
+    console_log!("Test console.log");
     alert("Hello, it's tfidf time once again!");
 }
